c3db0605a55083179bae451294a4e0ae9fbf270e
Ricky Shi <ricky.shi@accenture.com>
2024-02-21 17:35:51 +0000

    feat: get commits

--- src/commit.js
+++ src/commit.js
@@ -1,17 +1,23 @@
 const github = require('@actions/github')
 
 async function listCommits(token) {
-  console.log('Listing commits...')
-
   const octokit = github.getOctokit(token)
   const { owner, repo } = github.context.repo
   const pull_number = github.context.payload.number
 
-  return await octokit.rest.pulls.listCommits({
+  const res = await octokit.rest.pulls.listCommits({
     owner,
     repo,
     pull_number
   })
+
+  if (!res.data || res.data.length === 0) {
+    throw new Error(
+      `No commits found for owner=${owner}, repo=${repo}, pull_number=${pull_number}`
+    )
+  }
+
+  return res.data
 }
 
 module.exports = {
