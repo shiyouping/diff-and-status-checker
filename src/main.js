849f0f9771dca6bcd47b027a00efa700dbfd74a1
Ricky Shi <ricky.shi@accenture.com>
2024-02-21 15:50:32 +0000

    feat: add the initalization

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const listCommits = require('.listCommits.js')
 
 /**
  * The main function for the action.
@@ -7,20 +7,15 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const token = core.getInput('token')
+    const commits = await listCommits(token)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    for (const commit of commits) {
+      console.log(JSON.stringify(commit))
+    }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('hasDiff', 'true')
   } catch (error) {
-    // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
 }
