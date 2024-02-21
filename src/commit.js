f59ce8de9264dc5bcaa7afb0db48ee856cf0b59d
Ricky Shi <ricky.shi@accenture.com>
2024-02-21 16:25:18 +0000

    fix: add missing dependency

--- src/commit.js
+++ src/commit.js
@@ -3,6 +3,9 @@ const github = require('@actions/github')
 async function listCommits(token) {
   const octokit = github.getOctokit(token)
   const { owner, repo, pull_number } = github.context.issue
+
+  console.log(JSON.stringify(github.context))
+
   return await octokit.rest.pulls.listCommits({
     owner,
     repo,
