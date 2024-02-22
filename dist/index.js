0185b002bb1b44cd19ec79ef281dfdc968557da8
Ricky Shi <ricky.shi@accenture.com>
2024-02-22 22:08:19 +0000

    build: update compiled file

--- dist/index.js
+++ dist/index.js
@@ -13127,7 +13127,7 @@ const getShas = async () => {
             per_page: 250,
             page
         });
-        core.info(`****** Commits: ${res}`);
+        core.info(`****** Commits: ${JSON.stringify(res)}`);
         allCommits.push(...res.data);
         page++;
     } while (res.data.length);
