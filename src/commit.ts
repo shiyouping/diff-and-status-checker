b43c55e006a16154d80223f33d8bec0105a19dde
Ricky Shi <ricky.shi@accenture.com>
2024-02-22 14:00:05 +0000

    test: add logs

--- src/commit.ts
+++ src/commit.ts
@@ -14,18 +14,27 @@ export const listCommits = async (): Promise<
     `Listing commits for owner: ${owner}, repo: ${repo}, pullNumber: ${pullNumber}`
   )
 
-  const res = await octokit.rest.pulls.listCommits({
-    owner,
-    repo,
-    pull_number: pullNumber,
-    per_page: 250,
-    page: 10
-  })
+  const allCommits = []
+  let res
+  let page = 0
 
-  // FIXME
-  core.info(`********** Commit response: ${JSON.stringify(res)}`)
+  do {
+    // FIXME
+    core.info(`Page: ${page}`)
 
-  if (!res?.data?.length) {
+    res = await octokit.rest.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: pullNumber,
+      per_page: 5,
+      page
+    })
+
+    allCommits.push(...res.data)
+    page++
+  } while (res.data.length)
+
+  if (!allCommits.length) {
     throw new Error(
       `No commits found for owner: ${owner}, repo: ${repo}, pullNumber: ${pullNumber}`
     )
